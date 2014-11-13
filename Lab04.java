19c17919786928ff318ca75cea2a6def3dbe4f16
samrooney <sam_rooney@asl.org>
2014-11-13 11:58:27 +0000

    done

--- Lab04.java
+++ Lab04.java
@@ -11,7 +11,84 @@ import edu.fcps.karel2.Robot;
 
 public class Lab04 {
 
-     public static void main(String[] args) {
-          // TODO Your code goes here
-     }
-}
+  public static void takeTheField(Athlete arg)
+  {
+	arg.move();
+	arg.move();
+	arg.move();
+	arg.move();
+	arg.turnRight();
+	arg.move();
+	arg.move();
+   }
+
+public static void main(String[] args) {
+            
+            Display.openWorld("maps/arena.map");
+            Display.setSize(10, 10);
+            Display.setSpeed(8);
+          
+               Athlete Jackie = new Athlete();
+               Athlete Derek = new Athlete();
+               Athlete Pete = new Athlete();
+               Athlete Johnny = new Athlete();
+               Athlete Bob = new Athlete();
+               Athlete Coach = new Athlete(1,7, Display.EAST, 0);
+               Athlete JackNavinWeinstein = new Athlete();
+               
+               takeTheField(Jackie);
+               takeTheField(Derek);
+               takeTheField(Pete);
+               takeTheField(Johnny);
+               takeTheField(Bob);
+               takeTheField(JackNavinWeinstein);
+             
+               Jackie.move();
+               Jackie.move();
+               Jackie.move();
+               Jackie.turnLeft();
+               Jackie.move();
+               Jackie.move();
+               Jackie.turnAround();
+               
+             
+               Derek.move();
+               Derek.turnLeft();
+               Derek.move();
+               Derek.turnAround();
+               
+               Pete.move();
+               Pete.move();
+               Pete.move();
+               Pete.move();
+               Pete.move();
+               Pete.turnLeft();
+               Pete.move();
+               Pete.turnAround();
+               
+               Johnny.move();
+               Johnny.move();
+               Johnny.turnRight();
+               
+               Bob.move();
+               Bob.move();
+               Bob.move();
+               Bob.turnRight();
+               
+               JackNavinWeinstein.move();
+               JackNavinWeinstein.move();
+               JackNavinWeinstein.move();
+               JackNavinWeinstein.move();
+               JackNavinWeinstein.turnRight();
+               
+               
+               
+             
+               
+             
+          }
+          
+          
+          }
+               
+      
